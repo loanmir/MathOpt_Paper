10f1b31146ae621b340a3f0c09fc9fae00c30af1
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2025-06-16 09:23:43 +0200

    multiple_index_vars_test

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-# TEST CHECKING IF GUROBI WORKS!
-
 import gurobipy as gb
-
-# Problem 1
-
-item_weight = [10, 15, 17, 7, 9, 13, 4, 20]
-box_weight = [15, 20, 30, 35, 40]
-max_weight = 100
-
-n_items = len(item_weight)
-m_boxes = len(box_weight)
-
-knapsack = gb.Model("Box Packing")
-
-x = knapsack.addVars([i for i in range(n_items)],[j for j in range(m_boxes)], vtype=gb.GRB.BINARY, name="x")
-y = knapsack.addVars([j for j in range(m_boxes)], vtype=gb.GRB.BINARY)
+import numpy as np
 
 
-#1st constraint - In every box on item MUST be picked exactly once (AT MOST 1)
-for i in range(n_items):
-    knapsack.addConstr(
-        gb.quicksum(x[i, j] for j in range(m_boxes)) == 1, name=f"Item_{i}_Placement"
-    )
+# Initializing the model
+ILP_Model = gb.Model("Electric_Bus_Model")
 
-# 2nd constraint - sum of weights of each item cannot exceed the max weight = 100
-knapsack.addConstr(
-     gb.quicksum(item_weight[i] * x[i, j] for i in range(n_items) for j in range(m_boxes)) <= max_weight
-)
+# Parameters
+t = []
+j = []
+r = []
+b = []
+c = []
+s = []
+
+
+
+
+# Quantity of new buses variables
+nb_rbc = ILP_Model.addVars(r, b, c, vtype=gb.GRB.BINARY, name="nb_rbc")
+
+
+
+
+
+x = ILP_Model.addVars([i for i in range(n_items)],[j for j in range(m_boxes)], vtype=gb.GRB.BINARY, name="x")
+y = ILP_Model.addVars([j for j in range(m_boxes)], vtype=gb.GRB.BINARY)
+
 
 
 #3rd constraint - Cannot exceed weight of boxes!
 for j in range(m_boxes):
-    knapsack.addConstr(
+    ILP_Model.addConstr(
         gb.quicksum(item_weight[i] * x[i, j] for i in range(n_items)) <= box_weight[j] * y[j]
     )
 
 # Objective function
-knapsack.setObjective(
+ILP_Model.setObjective(
     gb.quicksum(y[j] for j in range(m_boxes)), sense=gb.GRB.MINIMIZE
 )
 
-knapsack.optimize()
+ILP_Model.optimize()
 
 # Print results
-if knapsack.status == gb.GRB.OPTIMAL:
-    print(f"\nMinimum number of boxes required: {int(knapsack.objVal)}")
+if ILP_Model.status == gb.GRB.OPTIMAL:
+    print(f"\nMinimum number of boxes required: {int(ILP_Model.objVal)}")
 
     for j in range(m_boxes):
         if y[j].x > 0.5:  # Box is used
