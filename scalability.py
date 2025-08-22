6babb39021fa77f3895bc03d5c6e4126d1edffb9
loanmir <lucas.jakin02@gmail.com>
2025-08-22 11:29:51 +0200

    improvements to scalability.py

--- scalability.py
+++ scalability.py
@@ -60,9 +60,9 @@ def run_scalability(scaling_steps=4):
     for step in range(1, scaling_steps + 1):
         # Scale problem size (increase bus/charger counts etc.)
         data_obj = data(
-            n_types_chargers=2 + 1,
-            n_types_elec_buses=10 + 5,
-            n_types_non_battery_buses=2 + 5,
+            n_types_chargers=2,
+            n_types_elec_buses= 10 + ((step-1) * 3),
+            n_types_non_battery_buses= 2 + ((step-1) * 3),
             up_j_value=10,
             uc_c_value=15,
         )
@@ -70,11 +70,11 @@ def run_scalability(scaling_steps=4):
 
         # Creating optimization instances
         instance_algorithm = OptimizationInstance(data_obj)
-        instance_HR = OptimizationInstance(data_obj)
+        #instance_HR = OptimizationInstance(data_obj)
         #instance_HRBC = OptimizationInstance(data_obj)
 
         # Set parameters for fair comparison
-        for inst in [instance_algorithm, instance_HR, ]:            #instance_HRBC
+        for inst in [instance_algorithm]:            # instance_HR, instance_HRBC -> this need to be added
             inst.model.setParam("OutputFlag", 0)  # keep console clean
             inst.model.setParam("MIPGap", 0)
             inst.model.setParam("IntFeasTol", 1e-9)
@@ -83,12 +83,12 @@ def run_scalability(scaling_steps=4):
 
         # Solving each problem variant
         model_algorithm = instance_algorithm.solve_algorithm()
-        model_HR = instance_HR.solve_heuristic_HR()
+        #model_HR = instance_HR.solve_heuristic_HR()
         #model_HRBC = instance_HRBC.solve_heuristic_HRBC()
 
         # Collect results
         results.append(solve_and_get_details(model_algorithm, "Algorithm", size_label))
-        results.append(solve_and_get_details(model_HR, "HR Heuristic", size_label))
+        #results.append(solve_and_get_details(model_HR, "HR Heuristic", size_label))
         #results.append(solve_and_get_details(model_HRBC, "HRBC Heuristic", size_label))
 
     return results
